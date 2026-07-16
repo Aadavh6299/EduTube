4ed4ac72b7ccecdc063bea02e98205567a1fbc56
Aadavh6299 <aadavh6299@gmail.com>
2026-07-16 14:52:21 +0530

    Add new channel IDs for educational content

--- src/data/channels.js
+++ src/data/channels.js
@@ -15,6 +15,8 @@ export const LEVELS = [
       "UCU0kWLAbhVGxXarmE3b8rHg", // Khan Academy India
       "UCi-J9CCaQ8w427GPHmoQGHA", // Vedantu Class 6, 7 & 8
       "UCMY7ZvLB6-DnuSis_2s37_A", // Vedantu Class 9 & 10
+      "UCT0s92hGjqLX6p7qY9BBrSA", // NCERT OFFICIAL
+      "UC3HS6gQ79jjn4xHxogw0HiA", // Magnet Brains
     ],
   },
   {
@@ -23,6 +25,13 @@ export const LEVELS = [
     emoji: "🏆",
     channelIds: [
       "UClaQJq84XMtMkL44zDmL-Tg", // Vedantu JEE
+      "UCiGyWN6DEbnj2alu7iapuKQ", // Physics Wallah
+      "UCzLqOSZPtUKrmSEnlH4LAvw", // Drishti IAS
+      "UCrC8mOqJQpoB7NuIMKIS6rQ", // StudyIQ IAS
+      "UC1L2JoMpcY6MRLhFd3gg5Xg", // Adda247
+      "UCZNNx4KYmCkwxCLdsHyWqQA", // Unacademy JEE
+      "UCw4wosjC-DKq95xI5klz92w", // Vision IAS
+      "UCgUeJ2Gv7rJtMBkQF2Ppn4A", // ALLEN Career Institute
     ],
   },
   {
