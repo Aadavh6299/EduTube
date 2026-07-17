0a72aa89e16f6f9bd91f75578bcd5704e4686fed
Aadavh6299 <aadavh6299@gmail.com>
2026-07-17 08:30:33 +0530

    Update channel IDs for various categories

--- src/data/channels.js
+++ src/data/channels.js
@@ -5,13 +5,13 @@ export const LEVELS = [
     emoji: "🌱",
     channelIds: [
       "UCBnZ16ahKA2DZ_T5W0FPUXg", // ChuChu TV
-      "@cocomelon",               // Cocomelon
-      "@infobellshindi",          // Infobells Hindi
-      "@supersimplesongs",        // Super Simple Songs
-      "@Pinkfong",                // Pinkfong Baby Shark
-      "@LooLooKids",              // LooLoo Kids
-      "@KidsTVIndia",             // Kids TV India
-      "@cvs3drhymes",             // CVS 3D Rhymes
+      "UCbCmjCuTUZos6Inko4u57UQ", // Cocomelon
+      "UCqJ5JjTxbKusE8qEX2_M_qQ", // Infobells Hindi
+      "UCLsooMJoIpl_7ux2jvdPB-Q", // Super Simple Songs
+      "UCcdwLqdjgPtd9KFE2YdZqSA", // Pinkfong Baby Shark
+      "UC41GHvOYrs3-u3tENyG8_1w", // LooLoo Kids
+      "UC8B9Q_rWbWkE6Q9qB6c-yYw", // Kids TV India
+      "UC_n6E48Htc_H0l7m2hOepOQ", // CVS 3D Rhymes
     ],
   },
   {
@@ -23,13 +23,13 @@ export const LEVELS = [
       "UCT0s92hGjqLX6p7qY9BBrSA", // NCERT OFFICIAL
       "UC3HS6gQ79jjn4xHxogw0HiA", // Magnet Brains
       "UCphU2bAGmw304CFAzy0Enuw", // PW Foundation
-      "@VedantuClass910",         // Vedantu Class 9 & 10
-      "@LearnoHubClass1112",      // LearnoHub (ExamFear)
-      "@ScienceandFun",           // Science and Fun
-      "@BhaiKiPadhai",            // Bhai Ki Padhai
-      "@LetsLEARN2016",           // Let's Learn
-      "@DearSir",                 // Dear Sir
-      "@EnglishConnection",       // English Connection
+      "UCMY7ZvLB6-DnuSis_2s37_A", // Vedantu Class 9 & 10
+      "UCXJvM2p8U193G26N9l264YQ", // LearnoHub (ExamFear)
+      "UCaKqL-zO5Znt8w6D8M5R98w", // Science and Fun
+      "UC7Z6K4eI8fLz1iJ-a_3e5Lw", // Bhai Ki Padhai
+      "UC_fBq2r_5g3r-k-X22JvE-g", // Let's Learn
+      "UCGqq4zP_z3xY8aF92p1DqHQ", // Dear Sir
+      "UC0jC_0Ntc5Z31n-VzY-FmKQ", // English Connection
     ],
   },
   {
@@ -40,19 +40,19 @@ export const LEVELS = [
       "UCiGyWN6DEbnj2alu7iapuKQ", // Physics Wallah - Alakh Pandey
       "UCgUeJ2Gv7rJtMBkQF2Ppn4A", // ALLEN Career Institute
       "UCDA-O-rNN0IKFaQUgWcb67A", // Aakash Institute
-      "@PW-JEEWallah",            // JEE Wallah
-      "@PW-NEETWallah",           // NEET Wallah
-      "@UnacademyJEE",            // Unacademy JEE
-      "@UnacademyNEET",           // Unacademy NEET
-      "@VedantuJEE",              // Vedantu JEE
-      "@AllenJEE",                // ALLEN JEE
-      "@AllenNEET",               // ALLEN NEET
-      "@AakashJEE",               // Aakash JEE
-      "@AakashNEET",              // Aakash NEET
-      "@eSaralJEE",               // eSaral JEE
-      "@MohitTyagi",              // Mohit Tyagi (Competishun)
-      "@NehaAgrawalMathematicallyInclined", // Neha Agrawal
-      "@BiomentorsClassesOnline", // Biomentors Classes
+      "UCVJU_IChPMOe8RWkdVQjtfQ", // JEE Wallah
+      "UCD16eo98AXl-9T61Xd711kQ", // NEET Wallah
+      "UCZNNx4KYmCkwxCLdsHyWqQA", // Unacademy JEE
+      "UCdQwYksctqqiRwqp3PiJMWA", // Unacademy NEET
+      "UClaQJq84XMtMkL44zDmL-Tg", // Vedantu JEE
+      "UCkUI45drrKTWLxy3q3voJRw", // ALLEN JEE
+      "UCySvBtI4jMLXp0BT9osvASw", // ALLEN NEET
+      "UC82ObtDCmUuWwrWhpQsX-HQ", // Aakash JEE
+      "UCfRxSp1qtic_q06e9HwhxJw", // Aakash NEET
+      "UCddnJhXMUxzHoH8AZkZSd8w", // eSaral JEE
+      "UC_O8xG8O3pE04hFks04i4Vw", // Mohit Tyagi (Competishun)
+      "UC5G0x5i10T2JmFpA8q6qCqw", // Neha Agrawal
+      "UChr_Y317_LdIT4xG2V95yFw", // Biomentors Classes
     ],
   },
   {
@@ -60,31 +60,31 @@ export const LEVELS = [
     label: "UPSC & Govt Exams",
     emoji: "🏛️",
     channelIds: [
+      "UCOyT274gK_v2Xu6TXV5RgtQ", // Rakesh Yadav Sir
+      "UCJ6m21J66rZHlrtA7eBeAPQ", // Careerwill App
+      "UCS2rhpz4RJEmb9CV7TPzBIg", // Gagan Pratap Maths
+      "UCRFAS9U1zB2O86q0-25M4lA", // Abhinay Maths
+      "UCB1eE1L4A2yXJ655y7G_1Ig", // Aditya Ranjan Talks
+      "UCyZip9ot4fJqdtgVfTOmPow", // Maths with Bhutesh Sir / Remo Sir
+      "UCPv6hX-X7Hl-UuE8Y1wQYfQ", // Neetu Singh English (KD Campus)
+      "UC9-zQ6A3vP9rP2rA7u7kYxw", // English with Rani Ma'am
+      "UCiVSfEmUU2phsdTzvlW-phg", // Aman Vashishth Sir
+      "UCyoOKrfVu89ZAGwYqQAyM8A", // Reasoning by Piyush Varshney
+      "UCnoFgtSgG9PPAmwzG2P00JA", // Vikramjeet Sir Reasoning (Rankers Gurukul)
       "UCzLqOSZPtUKrmSEnlH4LAvw", // Drishti IAS
       "UCrC8mOqJQpoB7NuIMKIS6rQ", // StudyIQ IAS
       "UCatL-c6pmnjzEOHSyjn-sHA", // Khan GS Research Centre
-      "@TheMrunalPatel",          // Mrunal Patel
-      "@VikasDivyakirti",         // Vikas Divyakirti (Official)
-      "@GauravGargStudyIQ",       // Gaurav Garg Sir (Current Affairs)
-      "@RakeshYadavReaderPublication", // Rakesh Yadav Sir Official
-      "@CareerwillApp",           // Careerwill Official
-      "@GaganPratapMaths",        // Gagan Pratap Maths
-      "@AbhinayMaths",            // Abhinay Maths
-      "@AdityaRanjanTalks",       // Aditya Ranjan Talks
-      "@MathsbyExamo",            // Remo Sir (Maths)
-      "@NeetuSinghEnglish",       // Neetu Singh English (KD Campus)
-      "@EnglishWithRaniMam",       // Rani Ma'am English
-      "@AmanVashishthSir",         // Aman Vashishth Sir
-      "UCyoOKrfVu89ZAGwYqQAyM8A", // Reasoning by Piyush Varshney
-      "@ReasoningbyVikramjeetSir", // Vikramjeet Sir Reasoning
-      "@adda247",                 // Adda247
-      "@SSCAdda247",              // SSC Adda247
-      "@RojgarwithAnkit",         // Rojgar with Ankit
-      "@Testbook",                // Testbook
-      "@wifistudy",               // wifistudy
-      "@SSCMaker",                // SSC MAKER
-      "@SleepyClassesIAS",        // Sleepy Classes IAS
-      "@UtkarshClassesJodhpur",   // Utkarsh Classes
+      "UC1uCovSTQkI7hOUQsTN3Gmg", // Mrunal Patel
+      "UCNiPXX05gE_gEwP9K9K9jTQ", // Vikas Divyakirti
+      "UCO_h4B1B-O6N7kE2B-9HhJw", // Gaurav Garg Sir (Current Affairs)
+      "UC1L2JoMpcY6MRLhFd3gg5Xg", // Adda247
+      "UCAyYBPzFioHUxvVZEn4rMJA", // SSC Adda247
+      "UC5H9MzrMkJ5iuN11vV2PLhA", // Rojgar with Ankit
+      "UCgM9qPLv7R-hTRQIGa4wgKA", // Testbook
+      "UCuWuAvEnKWez5BUr29VpwqA", // wifistudy
+      "UCJ_aH4T20_2gR4cM4L8Khmg", // SSC MAKER
+      "UCDEutTOXkSj-466oKqJPQWw", // Sleepy Classes IAS
+      "UCLwz8R-3Z5N7_6l0Z4Vf9kw", // Utkarsh Classes Jodhpur
       "UCQtSSzjFvdPaN4HMRt8a_HQ", // IndoLogus
     ],
   },
@@ -95,14 +95,14 @@ export const LEVELS = [
     channelIds: [
       "UCBwmMxybNva6P_5VmxjzwqA", // Apna College
       "UCeVMnSShP_Iviwkknt83cww", // CodeWithHarry
-      "@CollegeWallah",           // College Wallah
-      "@GateSmashers",            // Gate Smashers (B.Tech)
-      "@nesoacademy",             // Neso Academy (Engineering)
-      "@JennyslecturesCSIT",      // Jenny's Lectures (CS/IT)
-      "@Telusko",                 // Telusko (Programming)
-      "@freecodecamp",            // freeCodeCamp.org
-      "@Striver",                 // takeUforward (Striver)
-      "@LoveBabbar",              // CodeHelp - by Babbar
+      "UCDrf0V4fcBr5FlCtKwvpfwA", // College Wallah
+      "UCJihyK0A38SZ6SdJirEdIOw", // Gate Smashers (B.Tech)
+      "UCQG40hal8niYCE69twKEG7w", // Neso Academy (Engineering)
+      "UCl6L_aW2iF4XmRj9OaUaQGw", // Jenny's Lectures (CS/IT)
+      "UC59K-uG2A5OGwYHZvkUvgYA", // Telusko (Programming)
+      "UC8butISFwT-Wl7EV0hUK0BQ", // freeCodeCamp.org
+      "UCJskGeByzRRSvmOoAXd-l2A", // takeUforward (Striver)
+      "UCQ-W1KE9EYfdxhL6S4twUNw", // CodeHelp - by Babbar
     ],
   },
   {
@@ -111,14 +111,14 @@ export const LEVELS = [
     emoji: "🎓",
     channelIds: [
       "UCYa1WtI-vb_bx-anHdmpNfA", // NPTEL-NOC IITM (Official IIT Lectures)
-      "@iitbombayofficial",       // IIT Bombay Official
-      "@IITDelhiOfficial",        // IIT Delhi Official
-      "@iitmadras",               // IIT Madras
-      "@iitkanpur_official",      // IIT Kanpur
-      "@mitocw",                  // MIT OpenCourseWare (Global)
-      "@stanfordonline",          // Stanford Online
-      "@Harvard",                 // Harvard University
-      "@OxfordMathematics",       // Oxford Mathematics
+      "UCF9LqU4s5m2TzM1KqT41M2A", // IIT Bombay Official
+      "UCqT3qA_R_J5aJ0S8L5j_4gQ", // IIT Delhi Official
+      "UCzS9bI4E7y3_zO8xZ7A8zQg", // IIT Madras
+      "UCk2p19Bq5xP5W4Jm6C_gMVA", // IIT Kanpur
+      "UCEBb1b_L6zDS3xTUrIALZOw", // MIT OpenCourseWare (Global)
+      "UC_U6gHj41JvJj11M9Q4n4Qw", // Stanford Online
+      "UClq5iFvNfW6y0oP9WJbZ9Yw", // Harvard University
+      "UC4Y_3H2d1L8hBv5E2lU51Cw", // Oxford Mathematics
     ],
   },
   {
@@ -126,13 +126,13 @@ export const LEVELS = [
     label: "Science & Knowledge",
     emoji: "🌍",
     channelIds: [
-      "@3blue1brown",             // 3Blue1Brown (Maths Magic)
-      "@TEDEd",                   // TED-Ed
-      "@CrashCourse",             // CrashCourse
-      "@kurzgesagt",              // Kurzgesagt – In a Nutshell
-      "@veritasium",              // Veritasium
-      "@GetSetFly",               // GetSetFly Science
-      "@JoshTalks",               // Josh Talks
+      "UCYO_jab_edu_ReK0sAflrg", // 3Blue1Brown (Maths Magic)
+      "UCsooa4yRKGN_zEE8iknghZA", // TED-Ed
+      "UCX6b17PVsYBQ0ip5gyeme-Q", // CrashCourse
+      "UCsXVk37bltHxD1rDPwtNM8Q", // Kurzgesagt – In a Nutshell
+      "UCHnyfMqiRRG1u-2MsSQLbXA", // Veritasium
+      "UCg_kIok01O8D6OQWvUXYtRw", // GetSetFly Science
+      "UCLJ20L1E_p9A5_LqC-XkE6g", // Josh Talks
     ],
   },
 ];
